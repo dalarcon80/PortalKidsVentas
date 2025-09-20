0874bfbf084e82875384b9fe9b300fb409dd00da
esteban alarcon <esteban.alarconrmaz@gmail.com>
2025-09-20 18:10:43 -0500

    ssss

--- students/ealarconm1/scripts/m3_explorer.py
+++ students/ealarconm1/scripts/m3_explorer.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv(r'C:\Users\sarac\Ventas\PortalKidsVentas\students\ealarconm1\data\bronze\orders.csv', sep= ',')
+df = pd.read_csv(r'C:\Users\sarac\Ventas\PortalKidsVentas\students\ealarconm1\data\bronze\products.csv', sep= ',')
 
 print(df.head())
