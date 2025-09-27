ac5640e3193555b2167edede71a9e29088935335
esteban alarcon <esteban.alarconrmaz@gmail.com>
2025-09-27 18:52:40 -0500

    fff

--- students/ealarconm1/scripts/m3_explorer.py
+++ students/ealarconm1/scripts/m3_explorer.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv(r'C:\Users\sarac\Ventas\PortalKidsVentas\students\ealarconm1\data\bronze\products.csv', sep= ',')
+df = pd.read_csv(r'C:\Users\sarac\Ventas\PortalKidsVentas\students\ealarconm1\data\bronze\customers.csv', sep= ',')
 
 print(df)
 
-print("Shape:",df.shape)
+print(f"Shape: df.shape")
