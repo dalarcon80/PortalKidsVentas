ae4874bfa5027329fb84ce34d74dd5e70beca9f6
esteban alarcon <esteban.alarconrmaz@gmail.com>
2025-09-27 20:00:34 -0500

    dsdfffggg?

--- students/ealarconm1/scripts/m3_explorer.py
+++ students/ealarconm1/scripts/m3_explorer.py
@@ -3,4 +3,7 @@ import pandas as pd
 df = pd.read_csv(r'C:\Users\sarac\Ventas\PortalKidsVentas\students\ealarconm1\data\bronze\customers.csv', sep= ',')
 
 print("shape:",df.shape)
-
+print(df.head(8))
+print(df.dtypes)
+print(df.columns.tolist())
+print(len(df))
